b611b4b7dae5b73a05ebcbd66ce731f030c98571
vickyyy-t <vickytaozi@outlook.com>
2025-10-02 18:37:10 -0400

    retrieve_done

--- src/main/java/NumberTriangle.java
+++ src/main/java/NumberTriangle.java
@@ -90,8 +90,17 @@ public class NumberTriangle {
      *
      */
     public int retrieve(String path) {
-        // TODO implement this method
-        return -1;
+        if (path.isEmpty()) {
+            return root;
+        }
+        char dir = path.charAt(0);
+        if (dir == 'l' && left != null) {
+            return left.retrieve(path.substring(1));
+        } else if (dir == 'r' && right != null) {
+            return right.retrieve(path.substring(1));
+        } else {
+            throw new IllegalArgumentException("Invalid path: " + path);
+        }
     }
 
     /** Read in the NumberTriangle structure from a file.
